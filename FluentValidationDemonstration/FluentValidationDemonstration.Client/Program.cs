e6afc6f58dbd36fde8f231cd2781e0ca2b93a598
nhwilly <billnoel@outlook.com>
2025-11-04 09:43:29 -0500

    save

--- FluentValidationDemonstration/FluentValidationDemonstration.Client/Program.cs
+++ FluentValidationDemonstration/FluentValidationDemonstration.Client/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FluentValidation;
+using Morris.Blazor.Validation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddFormValidation(config =>
+  config
+    .AddDataAnnotationsValidation()
+    .AddFluentValidation(typeof(Program).Assembly)
+);
 await builder.Build().RunAsync();
